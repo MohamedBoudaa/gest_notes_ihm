f0f25439018c311213be73c055e81d327185b9cf
MohamedBoudaa <60492162+MohamedBoudaa@users.noreply.github.com>
2020-12-05 19:14:45 +0100

    Login Form
    added login form
    
    Core dependency in pom

---
+++ src/main/java/com/gui/AppMainFrame.java
@@ -0,0 +1,42 @@
+package com.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AppMainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppMainFrame frame = new AppMainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AppMainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1041, 715);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ src/main/java/com/gui/LoginPannel.java
@@ -0,0 +1,104 @@
+package com.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JTextArea;
+import java.awt.SystemColor;
+
+public class LoginPannel extends JFrame {
+
+	private JPanel formPan;
+
+	private static final String URL_RESOURCES = new String("src/main/java/com/resources/");
+	
+	private JLabel userLab = new JLabel("Username :");
+	private JTextField userF = new JTextField();
+	private JLabel pwLab = new JLabel("Password :");
+	private JPasswordField pwF = new JPasswordField();
+	private final JPanel panel = new JPanel();
+	private final JButton btn = new JButton("Connexion");
+	private JLabel logoLabel = new JLabel();
+	
+	/**
+	 * Create the frame.
+	 */
+	public LoginPannel() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 605, 388);
+		formPan = new JPanel();
+		formPan.setBackground(UIManager.getColor("Tree.textBackground"));
+		setTitle("Login");
+		formPan.setLayout(null);
+		panel.setBackground(UIManager.getColor("Tree.textBackground"));
+		panel.setBounds(139, 116, 318, 169);
+		
+		formPan.add(panel);
+		panel.setLayout(null);
+		userLab.setBounds(0, 0, 82, 89);
+		panel.add(userLab);
+		userF.setBorder(UIManager.getBorder("RadioButton.border"));
+		userF.setBounds(92, 31, 217, 26);
+		panel.add(userF);
+		pwLab.setBounds(0, 44, 82, 114);
+		panel.add(pwLab);
+		pwF.setBorder(UIManager.getBorder("RadioButton.border"));
+		pwF.setBounds(92, 88, 217, 26);
+		panel.add(pwF);
+		btn.setBorder(UIManager.getBorder("MenuBar.border"));
+		btn.setBackground(UIManager.getColor("PasswordField.selectionBackground"));
+		btn.setBounds(209, 135, 100, 23);
+		
+		panel.add(btn);
+		
+		getContentPane().add(formPan);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.decode("#005297"));
+		panel_1.setBounds(0, 0, 589, 115);
+		formPan.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel loginTitle = new JLabel("<html><body>Ecole Nationale des Sciences<br>Appliquées d'Al Hoceima</body></html>");
+		loginTitle.setForeground(UIManager.getColor("PasswordField.background"));
+		loginTitle.setFont(new Font("Courier New", Font.ITALIC, 25));
+		loginTitle.setBounds(152, 11, 466, 95);
+		panel_1.add(loginTitle);
+		
+		logoLabel.setBounds(0, 0, 151, 115);
+		ImageIcon icon = new ImageIcon(URL_RESOURCES + "logo-ensah.png");
+		
+		logoLabel.setIcon(icon);
+		panel_1.add(logoLabel);
+//		validate();
+	}
+	
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginPannel frame = new LoginPannel();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
