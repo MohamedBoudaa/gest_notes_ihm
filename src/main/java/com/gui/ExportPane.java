e6508bfb4e3f6462f158e979c12a32390953ce8f
youss999 <63669195+youss999@users.noreply.github.com>
2021-01-22 23:42:05 +0100

    Update ExportPane.java

--- src/main/java/com/gui/ExportPane.java
+++ src/main/java/com/gui/ExportPane.java
@@ -1,6 +1,19 @@
 package com.gui;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.bll.SearchManagerImpl;
 
 public class ExportPane extends JPanel {
 
@@ -9,9 +22,23 @@ public class ExportPane extends JPanel {
 	 */
 	public ExportPane() {
 
+		setSize(1003, 769);
+		setLayout(null);
+
 		
+		//Ajouter des composants graphiques
+		JPanel contentPane=new JPanel();
 		
-		setVisible(false);
+		JButton jButton=new JButton("Click me");
+		jButton.setBounds(200, 20, 100, 30);
+		contentPane.add( jButton);
+		JCheckBox jCheckbox=new JCheckBox("Je suis checkbox");
+		jCheckbox.setBounds(200, 60, 100, 30);
+		contentPane.add(jCheckbox);
+		JTextField jTextfield=new JTextField("Ecrire ici");
+		jTextfield.setBounds(200, 90, 100, 30);
+		contentPane.add(jTextfield);
+		setVisible(true);
 	}
 
 }
